5ab29bee43151ac2687ea8cf0cbad41218b6f8eb
Elizaveta Pyalova <93152706+LizaPyalova@users.noreply.github.com>
2023-01-16 18:24:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+ public class Main {
+            interface Phone {
+                void whatis();
+            }
+
+            static class Iphone implements Phone {
+                public void whatis() {
+                    System.out.println("I am Iphone");
+                }
+            }
+
+            static class Samsung implements Phone {
+                public void whatis() {
+                    System.out.println("I am Samsung");
+                }
+            }
+
+            abstract static class PhoneFactory {
+                abstract Phone createPhone();
+            }
+
+            static class IphoneFactory extends PhoneFactory {
+                Phone createPhone() {
+                    return new Iphone();
+                }
+            }
+
+            static class SamsungFactory extends PhoneFactory {
+                Phone createPhone() {
+                    return new Samsung();
+                }
+            }
+
+
+            public static void main(String[] args) {
+                PhoneFactory iphoneFactory = new IphoneFactory();
+                Phone iphone = iphoneFactory.createPhone();
+                iphone.whatis();
+
+                PhoneFactory samsungFactory = new SamsungFactory();
+                Phone samsung = samsungFactory.createPhone();
+                samsung.whatis();
+            }
+
+        }
